e078d5eeb86282502c389cd424739a03fd6a8efc
tokerson <J.Tokarzewski@stud.elka.pw.edu.pl>
2019-11-03 14:50:57 +0100

    Fix pagination when no results matching filters

--- Z01/PaginatedList.cs
+++ Z01/PaginatedList.cs
@@ -5,33 +5,36 @@ using System.Linq;
 namespace Z01
 {
     public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int pageIndex, int pageSize)
         {
-            public int PageIndex { get; private set; }
-            public int TotalPages { get; private set; }
-
-            public PaginatedList(List<T> items, int pageIndex, int pageSize)
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(items.Count / (double)pageSize);
+            if (TotalPages == 0) 
             {
-                PageIndex = pageIndex;
-                TotalPages = (int)Math.Ceiling(items.Count / (double)pageSize);
-
-                this.AddRange(items.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList());
-                Console.WriteLine("Paginated List " + this.First());
+                TotalPages = 1;
             }
+            items = items.Count != 0 ? items.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList() : new List<T>();
+            this.AddRange(items);
+        }
 
-            public bool HasPreviousPage
+        public bool HasPreviousPage
+        {
+            get
             {
-                get
-                {
-                    return (PageIndex > 1);
-                }
-            }
-
-            public bool HasNextPage
-            {
-                get
-                {
-                    return (PageIndex < TotalPages);
-                }
+                return (PageIndex > 1);
             }
         }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+    }
 }
